d05b24c1fc2948b1d72593fc49d4d2227f1419cc
misael.oliveira <misael.oliveira@syngenta.com>
2024-08-01 21:30:59 -0300

    creating vpc

---
+++ network/main.tf
@@ -0,0 +1,8 @@
+#provider "aws" {
+#    region = "${var.AWS_REGION}"
+#}
+
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+}
+
---
+++ network/vpc.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "baeta-vpc"
+  }
+}
+
+
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Baeta VPC IG"
+  }
+}
